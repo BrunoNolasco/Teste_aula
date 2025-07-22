f368fb0220ea6da6c3611350b8c36065163b1712
BrunoNolasco <brunonolasco2763@gmail.com>
2025-07-22 11:34:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-print("Olá turma do Python!")
-print("Tudo bem!")
-nome: str = input("Qual é o seu nome? ")
-print(f"Olá, {nome}!")
+def estado():
+    tudo_bem = input("Olá! Tudo bem? (sim ou não): ").strip().lower()
+
+    if tudo_bem == "sim":
+        print("Que legal, então vamos deixá-lo ainda melhor!")
+    elif tudo_bem == "não":
+        print("Que pena, mas não se preocupe, vamos melhorá-lo!")
+    else:
+        print("Resposta inválida. Por favor, responda com 'sim' ou 'não'.")
+
+estado()
+
+
+#
+#
+#
+#
+#
+#print("Olá pessoal!")
+#
+#
+#
+#
+#
+#
+#nome: str = input("Qual é o seu nome? ")
+#print(f"Olá, {nome}!")
+#
+#
+#print("Tudo bem?")
